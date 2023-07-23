7bbfcc95e329157846a077cc52e57ae71df1fc0e
ratulbasak <ratulbasak93@gmail.com>
2023-07-23 03:32:36 +0200

    removed unused code from workflow

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -4,6 +4,4 @@ mkdir ~/.ssh
 echo "$APPSERVER_SSH_KEY" > ~/.ssh/aws.pem
 chmod 0600 ~/.ssh/aws.pem
 
-ls -al ~/.ssh/
-
-ansible-playbook -i ansible/inventories/production ansible/playbook.yml -vvvv
+ansible-playbook -i ansible/inventories/production ansible/playbook.yml
