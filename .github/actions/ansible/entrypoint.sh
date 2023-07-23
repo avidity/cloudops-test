7b8f8bb618c81826669811919e1404fda65c8a8d
ratulbasak <ratulbasak93@gmail.com>
2023-07-23 03:01:08 +0200

    updated Dockerfile in ansible

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -4,4 +4,4 @@ mkdir ~/.ssh
 echo "$APPSERVER_SSH_KEY" > ~/.ssh/aws.pem
 chmod 0600 ~/.ssh/aws.pem
 
-ansible-playbook -i ansible/inventories/production ansible/playbook.yml
+ansible-playbook -i ansible/inventories/production ansible/playbook.yml -vvvv
