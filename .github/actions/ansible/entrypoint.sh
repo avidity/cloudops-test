1fb76ccaaaec7e948c6e800507c35df3dba20db3
ratulbasak <ratulbasak93@gmail.com>
2023-07-23 04:38:48 +0200

    fixed entrypoint.sh

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -4,4 +4,4 @@ mkdir ~/.ssh
 echo "$APPSERVER_SSH_KEY" > ~/.ssh/aws.pem
 chmod 0600 ~/.ssh/aws.pem
 
-ansible-playbook -i ansible/inventories/production ansible/playbook.yml "app_git_tag=$IMAGE_TAG"
+ansible-playbook -i ansible/inventories/production ansible/playbook.yml --extra-vars "app_git_tag=$IMAGE_TAG"
